f2062311be534a1a5ea9583d20581ccd475ee6c9
zalika9 <zalika9@students.d125.org>
2018-09-04 10:12:28 -0500

    Finish class project, continue working on emoji dictionary

--- MobileApps/Class Projects/VariablesChallengeStudent/VariablesChallenge/ViewController.swift
+++ MobileApps/Class Projects/VariablesChallengeStudent/VariablesChallenge/ViewController.swift
@@ -22,6 +22,7 @@ class ViewController: UIViewController
      ***************************************************/
     
     var lastName = "Ali-Khan"
+    var titleName = "Mr. "
     
     /***************************************************
      * End Your Code Here For Stretch #1 - Part I
@@ -31,6 +32,11 @@ class ViewController: UIViewController
      * Start Your Code Here For Stretch #2 - Part I
      ***************************************************/
     
+    var nameOne = "Player 1"
+    var nameTwo = "Player 2"
+    var nameThree = "Player 3"
+    var nameFour = "Player 4"
+    
     /***************************************************
      * End Your Code Here For Stretch #2 - Part I
      ***************************************************/
@@ -38,6 +44,11 @@ class ViewController: UIViewController
     /***************************************************
      * Start Your Code Here For Stretch #3 - Part I
      ***************************************************/
+    
+    var p1Score = 10
+    var p2Score = 40
+    var p3Score = 60
+    var p4Score = 30
     
     /***************************************************
      * End Your Code Here For Stretch #3 - Part I
@@ -59,34 +70,47 @@ class ViewController: UIViewController
          * Start Your Code Here For Stretch #1 - Part II
          ***************************************************/
         
+        var greeting = "Hello \(titleName)\(lastName)"
+        
         /***************************************************
          * End Your Code Here For Stretch #1 - Part II
          ***************************************************/
         
         //Uncomment the line below for Stretch #1
-        //stretchOneLabel.text = greeting
+        stretchOneLabel.text = greeting
         
         /***************************************************
          * Start Your Code Here For Stretch #2 - Part II
          ***************************************************/
+        
+        var teamOne = "\(nameOne), \(nameThree)"
+        var teamTwo = "\(nameTwo), \(nameFour)"
         
         /***************************************************
          * End Your Code Here For Stretch #2 - Part II
          ***************************************************/
         
         //Uncomment the line below for Stretch #2
-        //stretchTwoLabel.text = "Team 1: \(teamOne)\nTeam 2: \(teamTwo)"
+        stretchTwoLabel.text = "Team 1: \(teamOne)\nTeam 2: \(teamTwo)"
         
         /***************************************************
          * Start Your Code Here For Stretch #3 - Part II
          ***************************************************/
+        
+        var playerScore1 = "\(nameOne) : \(p1Score)"
+        var playerScore2 = "\(nameTwo) : \(p2Score)"
+        var playerScore3 = "\(nameThree) : \(p3Score)"
+        var playerScore4 = "\(nameFour) : \(p4Score)"
+        
+        var teamHighScoreOne = "\(playerScore1) & \(playerScore3)"
+        var teamHighScoreTwo = "\(playerScore2) & \(playerScore4)"
         
         /***************************************************
          * End Your Code Here For Stretch #3 - Part II
          ***************************************************/
         
         //Uncomment the line below for Stretch #1
-        //stretchThreeLabel.text = "\(teamHighScoreOne)\n\(teamHighScoreTwo)"
+        stretchThreeLabel.text = "\(teamHighScoreOne)\n\(teamHighScoreTwo)"
     }
     
 }
