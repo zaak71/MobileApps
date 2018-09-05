abe356c1d8bbb6a33d5c4878a87a5d5e70cfcc47
zaak71 <zaak7179@gmail.com>
2018-09-04 22:52:51 -0500

    almost finished hello world (need to add images)

--- MobileApps/Class Projects/ZuhairAli-Khan HelloWorld/ZuhairAli-Khan HelloWorld/ViewController.swift
+++ MobileApps/Class Projects/ZuhairAli-Khan HelloWorld/ZuhairAli-Khan HelloWorld/ViewController.swift
@@ -10,14 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bandInfoLabel: UILabel!
+    @IBOutlet weak var bandNameLabel: UILabel!
+    var favoriteBand = "Imagine Dragons"
+    var numberOfMembers = 4
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        bandNameLabel.text = favoriteBand
+        bandInfoLabel.text = "\(favoriteBand) has \(numberOfMembers) members."
     }
 
 
