5cb7d4db8e92b4bb881ea1e9a2e504b36e2a03fc
zaak71 <zaak7179@gmail.com>
2018-09-04 21:48:03 -0500

    Added Hello World class project to repository

---
+++ MobileApps/Class Projects/ZuhairAli-Khan HelloWorld/ZuhairAli-Khan HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZuhairAli-Khan HelloWorld
+//
+//  Created by Zuhair Ali-Khan on 9/4/18.
+//  Copyright © 2018 Zuhair Ali-Khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
